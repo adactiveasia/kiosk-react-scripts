8873dfd99e4d774acb17dd487fda287ebb9bab3d
Gethi <bigard.antoine@gmail.com>
2018-05-16 14:04:25 +0800

    Update clientApi service + bump 1.1.57

--- packages/react-scripts/template/src/services/ClientAPI.js
+++ packages/react-scripts/template/src/services/ClientAPI.js
@@ -22,6 +22,66 @@ class ClientAPI {
             cacheManager
         });
     }
+
+    getTagBy(filter){
+
+        return this.entityManager.getRepository("Tag").findBy(filter);
+
+    }
+
+    getPlaylistBy(filter){
+
+        return this.entityManager.getRepository("Playlist").findBy(filter);
+
+    }
+
+    getFile(id){
+
+        return this.entityManager.getRepository("File").get(id);
+
+    }
+
+    getMediasByPlaylistTag(tagName) {
+
+        let medias = [];
+
+        const tag = this.getTagBy({name: tagName});
+
+        if(tag.length > 0) {
+
+            const playlist = this.getPlaylistBy(
+
+                {
+
+                    tags: function (tags) {
+
+                        return tags.has(tag[0])
+
+                    }
+
+                }
+
+            );
+
+            if (playlist.length > 0) {
+
+                medias = this.entityManager.getRepository("Media").getList(playlist[0].medias.toJSON());
+
+
+
+                for(let i in medias) {
+
+                    medias[i].file = this.getFile(medias[i].file.value);
+
+                }
+
+            }
+
+        }
+
+        return medias;
+    }
+
 }
 
 const clientApi = new ClientAPI();
