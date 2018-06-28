475cd06cb02a93cd467e0a89115194cede5c58a2
Gethi <bigard.antoine@gmail.com>
2018-06-28 14:21:11 +0800

    Fix config-orverrides

--- packages/react-scripts/template/config-overrides.js
+++ packages/react-scripts/template/config-overrides.js
@@ -1,11 +1,12 @@
-const {getBabelLoader} = require('react-app-rewired');
+const path = require('path');
 
 module.exports = function override(config, env) {
     //do stuff with the webpack config...
 
     let babelLoader = getBabelLoader(config.module.rules);
+    const include = babelLoader.include;
     delete babelLoader.include;
-    babelLoader.exclude = /^((?!adactive).)*$/;
+    babelLoader.include = [include, path.resolve('node_modules/@adactive')]
 
     return config;
 };
