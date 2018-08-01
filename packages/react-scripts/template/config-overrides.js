fac8140cd28124d5f50da8b1f34c3895137487f9
Moroine Bentefrit <moroine.bentefrit@adactive.com>
2018-08-01 19:06:06 +0800

    Do not watch public folder

--- packages/react-scripts/template/config-overrides.js
+++ packages/react-scripts/template/config-overrides.js
@@ -3,17 +3,38 @@
 const path = require('path');
 const { getBabelLoader } = require('react-app-rewired');
 
-module.exports = function override(config /* , env */) {
-  // do stuff with the webpack config...
+module.exports = {
+    // The Webpack config to use when compiling your react app for development or production.
+    webpack: function (config /* , env */) {
+        // do stuff with the webpack config...
 
-  const babelLoader = getBabelLoader(config.module.rules);
-  const { include } = babelLoader;
-  delete babelLoader.include;
-  babelLoader.include = [
-    include,
-    path.resolve('node_modules/@adactive'),
-    path.resolve('node_modules/prex-es5'),
-  ];
+        const babelLoader = getBabelLoader(config.module.rules);
+        const { include } = babelLoader;
+        delete babelLoader.include;
+        babelLoader.include = [
+            include,
+            path.resolve('node_modules/@adactive'),
+            path.resolve('node_modules/prex-es5'),
+        ];
 
-  return config;
+        return config;
+    },
+    // The function to use to create a webpack dev server configuration when running the development
+    // server with 'npm run start' or 'yarn start'.
+    devServer: function (configFunction) {
+        // Return the replacement function for create-react-app to use to generate the Webpack
+        // Development Server config. "configFunction" is the function that would normally have
+        // been used to generate the Webpack Development server config - you can use it to create
+        // a starting configuration to then modify instead of having to create a config from scratch.
+        return function(proxy, allowedHost) {
+            // Create the default config by calling configFunction with the proxy/allowedHost parameters
+            const config = configFunction(proxy, allowedHost);
+
+            // Comment next line to watch changes in public folder
+            config.watchContentBase = false;
+
+            // Return your customised Webpack Development Server config.
+            return config;
+        }
+    }
 };
