8dff9631d35285db284e285ce14992a330d4a63e
Moroine Bentefrit <moroine.bentefrit@adactive.com>
2018-08-02 09:09:40 +0800

    Change startServer

--- packages/react-scripts/template/public/startServer.js
+++ packages/react-scripts/template/public/startServer.js
@@ -3,18 +3,16 @@ const serverOptions = require('./server/options');
 
 const updater = new DataUpdater();
 
-updater.update(339, {
-    "endpoint": "https://asia-api.adsum.io",
-    "username": "1056-device",
-    "key": "b6e8e6eaf2c7ff66b783e7721a57ed62f204c3bc3a68b729c1ea3a90a7c1e828"
-}).then(() => {
-    let server = new Server(serverOptions);
+updater
+    .update(__dirname)
+    .then(() => {
+        let server = new Server(serverOptions);
 
-    server
-        .start()
-        .then(
-            (server) => {
-            },
-            console.error,
-        );
-});
+        server
+            .start()
+            .then(
+                (server) => {
+                },
+                console.error,
+            );
+    });
